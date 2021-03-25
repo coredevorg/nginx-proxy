d31c330120ca65539aa798f1ab5386bc6384d3e1
Bernd Strebel <bernd.strebel@coredev.org>
2021-03-25 07:13:18 +0100

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 [ "$1" ] || { echo "usage: ./setup.sh <emil>" 1>&2 ; exit 1;  }
 [ -f ".env" ] || cp env.setup .env
-sed -i "s/{{DEFAULT_EMAIL}}/$1/g" .env
+sed -i -e "s/{{DEFAULT_EMAIL}}/$1/g" .env
 mkdir -p ./data/{certs,html,vhosts.d}
