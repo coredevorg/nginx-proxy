9114940ec3783f241edd1e6bf738ac31bb40c391
Bernd Strebel <bernd.strebel@coredev.org>
2021-03-25 20:10:18 +0100

    check external proxy network

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-[ "$1" ] || { echo "usage: ./setup.sh <emil>" 1>&2 ; exit 1;  }
+[ "$1" ] || { echo "usage: ./setup.sh <email>" 1>&2 ; exit 1;  }
 [ -f ".env" ] || cp env.setup .env
 sed -i -e "s/{{DEFAULT_EMAIL}}/$1/g" .env
 mkdir -p ./data/{certs,html,vhosts.d}
+docker network ls | grep -q nginx-proxy || docker network create nginx-proxy
