3ae45cdb59b61633f6af4deeb95ff9d78803298b
Bernd Strebel <bernd.strebel@coredev.org>
2021-03-27 05:12:48 +0100

    persitent nginx-proxy letsencrypt mounts

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 [ "$1" ] || { echo "usage: ./setup.sh <email>" 1>&2 ; exit 1;  }
 [ -f ".env" ] || cp env.setup .env
-sed -i -e "s/{{DEFAULT_EMAIL}}/$1/g" -e "s/{{NGINX_PROXY_PATH}}/$PWD/g" .env
-mkdir -p ./data/{certs,html,vhosts.d}
+sed -i -e "s/{{DEFAULT_EMAIL}}/$1/g" -e "s#{{NGINX_PROXY_PATH}}#$PWD#g" .env
+mkdir -p $PWD/data/{certs,html,vhosts.d,acme.sh,dhparam}
 docker network ls | grep -q nginx-proxy || docker network create nginx-proxy
