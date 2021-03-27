1b5e1b03a801453888cfea4c84d8712fd404c504
Bernd Strebel <bernd.strebel@coredev.org>
2021-03-27 09:20:24 +0100

    fixed type in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 [ "$1" ] || { echo "usage: ./setup.sh <email>" 1>&2 ; exit 1;  }
 [ -f ".env" ] || cp env.setup .env
 sed -i -e "s/{{DEFAULT_EMAIL}}/$1/g" -e "s#{{NGINX_PROXY_PATH}}#$PWD#g" .env
-mkdir -p $PWD/data/{certs,html,vhosts.d,acme.sh,dhparam}
+mkdir -p $PWD/data/{certs,html,vhost.d,acme.sh,dhparam}
 docker network ls | grep -q nginx-proxy || docker network create nginx-proxy
